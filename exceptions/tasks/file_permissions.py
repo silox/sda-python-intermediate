4685f33eca7a3ff6e0ba87c3c1d4155b0fdbb93a
silox <edruisl99@gmail.com>
2024-06-26 22:05:22 +0200

    Add exception tasks

---
+++ exceptions/tasks/file_permissions.py
@@ -0,0 +1,4 @@
+# Napis funkciu append_text(filename, text), ktora doplni text `text` do suboru s nazvom `filename`.
+# Ak je subor readonly, osetri tento pripad tak, ze vypises na vystup "Cannot write to file <filename>"
+# Pre windows userov, ako nastavit, aby bol subor readonly:
+# https://adamtheautomator.com/how-to-make-a-file-read-only/
---
+++ exceptions/tasks/uncontrollable_snake.py
@@ -0,0 +1,8 @@
+# Napis program, ktory bude kazdu sekundu vypisovat
+# f'Python {i} initiated...' pre i od 0 do nekonecna.
+# Ak uzivatel stlaci v konzoli ctrl+c, cim sa vyhodi `KeyboardInterrupt` vynimka,
+# program vypise 'You cannot stop me that easily!'
+# Je vhodne pouzit napriklad `time.sleep(1)` (`import time`), aby program cakal 1 sekundu.
+
+# Ako potom program vypnete?
+# Ostava ako uloha pre pozornych citatelov
