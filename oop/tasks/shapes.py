9d7875a3b8d6007b8c230dfd53f5e66237c6c29e
silox <edruisl99@gmail.com>
2024-06-27 22:12:11 +0200

    Change and improve triangle definition in  oop/shapes task

--- oop/tasks/shapes.py
+++ oop/tasks/shapes.py
@@ -1,5 +1,5 @@
 # Napis triedu Shape, ktora bude mat 2 abstraktne metody `perimeter()` a `area()`.
-# Dalej napis triedy Rectangle, Circle a Triangle, ktore dedia zo Shape.
+# Dalej napis triedy Rectangle, Circle a Triangle (jednostranny), ktore dedia zo Shape.
 # Kazda bude tieto 2 metody prepisovat a pocitat obvod, respektive obsah podla toho, co je to za utvar.
 # Init bude rozdielny podla toho, ake udaje o geometrickom tvare nas budu zaujimat.
 # HINT: Inspiruj sa ucebnymi materialmi
