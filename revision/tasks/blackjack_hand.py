7d32cbae87262e169d82e08e0814284b0b993834
silox <edruisl99@gmail.com>
2024-07-17 19:00:18 +0200

    Add blackjack task

---
+++ revision/tasks/blackjack_hand.py
@@ -0,0 +1,8 @@
+# Task
+# Napis program, ktory dostane na vstupe ruku dealera, dalej ruky hracov a zisti, kto vsetko vyhral v BlackJacku.
+# V pripade, ze maju vsetci cez 21 alebo nema nikto viac nez dealer, tak vsetci prehrali. V opacnom pripade
+# vyhral kazdy, kto ma viac nez dealer (pri rovnosti vyhrava dealer!).
+# Ak ma dealer cez 21, tak vyhrali vsetci, co nemaju cez 21.
+# Eso sa pocita za 1 alebo 11, ostatne hodnoty sa pocitaju za hodnotu na karte. Pre J, Q a K je to 10.
+# Kazdy sa snazi priblizit 21. Kto vsak ma viac, prehrava.
+# Karty na vstupe sa reprezentuju ako string v tvare {value}{suit}, ale inak je to na vas.
